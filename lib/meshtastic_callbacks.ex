8013586c6f230af5c1db43fa6874e4cfc61eb7f5
Davide Bettio <davide@uninstall.it>
2025-08-21 20:02:03 +0200

    meshtastic_callbacks: handle position messages
    Populate `meshtastic_position` micronesia table when receiving one, and
    add function for sending position.
    
    Signed-off-by: Davide Bettio <davide@uninstall.it>

--- lib/meshtastic_callbacks.ex
+++ lib/meshtastic_callbacks.ex
@@ -2,11 +2,32 @@ defmodule MeshtasticCallbacks do
   def init() do
     :micronesia.start()
     :micronesia.create_table(:meshtastic_message)
+    :micronesia.create_table(:meshtastic_position)
   end
 
   def message_cb(%{message: %{portnum: :TEXT_MESSAGE_APP, payload: payload}, packet_id: packet_id}) do
     IO.puts("Got text message: #{inspect(payload)}")
     :micronesia.dirty_write({:meshtastic_message, packet_id, payload})
+  end
+
+  def message_cb(%{
+        message: %{
+          portnum: :POSITION_APP,
+          payload:
+            %{
+              time: _time,
+              latitude_i: lat,
+              longitude_i: lon,
+              altitude: alt
+            } = payload
+        },
+        src: src
+      }) do
+    IO.puts("Got position message: #{inspect(payload)}")
+
+    :micronesia.dirty_write(
+      {:meshtastic_position, src, %{lat: lat * 0.0000001, lon: lon * 0.0000001, alt: alt}}
+    )
   end
 
   def message_cb(msg) do
@@ -16,6 +37,27 @@ defmodule MeshtasticCallbacks do
   def send_text_message(text) do
     data =
       %{portnum: :TEXT_MESSAGE_APP, payload: text}
+      |> :meshtastic_proto.encode()
+      |> :erlang.iolist_to_binary()
+
+    :ok = :meshtastic_server.send(:meshtastic_server, 0xFFFFFFFF, data)
+  end
+
+  def send_position(%{lat: lat, lon: lon, alt: alt}) do
+    time = :erlang.system_time(:second)
+
+    position_msg = %{
+      portnum: :POSITION_APP,
+      payload: %{
+        time: time,
+        latitude_i: round(lat * 10_000_000),
+        longitude_i: round(lon * 10_000_000),
+        altitude: round(alt)
+      }
+    }
+
+    data =
+      position_msg
       |> :meshtastic_proto.encode()
       |> :erlang.iolist_to_binary()
 
